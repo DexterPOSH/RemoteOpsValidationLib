207d26e317b5a45b81ed17554b05e9f6559f0d8e
DexterPOSH <dexter34925@yahoo.in>
2017-06-30 10:25:31 +0530

    Uses PSRemotely DSL

--- Microsoft/StorageSpacesDirect/S2DValidation.PSRemotely.ps1
+++ Microsoft/StorageSpacesDirect/S2DValidation.PSRemotely.ps1
@@ -1,29 +1,32 @@
+# Use the PSRemotely DSL to wrap the existing Pester tests and target remote nodes
+PSRemotely -Path "$PSScriptRoot\EnvironmentConfigData.psd1" {
+    
+    # All the nodes in the solution are to be targeted
+    Node $AllNodes.NodeName {
+        # Ensure that Server 2016 OS installation is done.
+        Describe "Operating system installed validation" {
+            $OS = Get-CimInstance -Class Win32_OperatingSystem
+            Context "Server Type check" {
+                It "Should have the OSType as WinNT" {
+                    $OS.OSType | Should Be 18
+                }
+            }
 
-
-# Ensure that Server 2016 OS installation is done.
-Describe "Operating system installed validation" -Tags OSConfigValidation {
-    $OS = Get-CimInstance -Class Win32_OperatingSystem
-    Context "Server Type check" {
-        It "Should have the OSType as WinNT" {
-            $OS.OSType | Should Be 18
+            Context 'Server 2016 check' {
+                It "Should have server 2016 installed" {
+                $OS.Caption | Should BeLike '*Server 2016*'
+                }
+            }
         }
-    }
 
-    Context 'Server 2016 check' {
-        It "Should have server 2016 installed" {
-        $Os.Caption | Should BeLike '*Server 2016*'
+        # Validate that the domain account is part of the local administrators group on a node.
+        Describe "Domain account is local administrator validation" {
+
+            $LocalGroupMember = Get-LocalGroupMember -Group Administrators -Member "DomainUser" -ErrorAction SilentlyContinue
+
+            It "Should be member of local admins group" {
+                $LocalGroupMember | Should NOT BeNullOrEmpty
+            }
         }
     }
 }
-
-
-# Validate that the domain account is part of the local administrators group on a node.
-Describe "Domain account is local administrator validation" {
-
-    $LocalGroupMember = Get-LocalGroupMember -Group Administrators -Member "DomainUser" -ErrorAction SilentlyContinue
-
-    It "Should be member of local admins group" {
-        $LocalGroupMember | Should NOT BeNullOrEmpty
-    }
-}
-
